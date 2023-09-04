e92ae2c8b92accfd5ed86164d5a44f513ce62074
James Holdren <jamesdholdren@gmail.com>
2023-09-04 15:47:34 -0400

    config: move lua setup into one file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,4 @@
 vim.o.termguicolors = true
-vim.cmd.colorscheme 'kanagawa-lotus'
 vim.o.syntax = 'on'
 vim.o.errorbells = false
 vim.o.smartcase = true
@@ -13,13 +12,14 @@ vim.o.hidden = true
 vim.o.completeopt='menuone,noinsert,noselect'
 vim.bo.autoindent = true
 vim.bo.smartindent = true
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.wo.number = true
 vim.wo.signcolumn = 'yes'
 vim.wo.wrap = false
+vim.cmd("colorscheme kanagawa")
 vim.api.nvim_set_option("clipboard","unnamed")
 
 local vim = vim
@@ -42,8 +42,8 @@ packer.startup(function()
   local use = use
   	use 'nvim-tree/nvim-tree.lua'
 	use "savq/melange-nvim"
-  use "rebelot/kanagawa.nvim"
-  use "neanias/everforest-nvim"
+    use "rebelot/kanagawa.nvim"
+    use "neanias/everforest-nvim"
 
 	use {
 	  'VonHeikemen/lsp-zero.nvim',
@@ -82,6 +82,7 @@ packer.startup(function()
 
       -- Configuring null-ls
       nls.setup({
+          debug = true,
           sources = {
               -- # FORMATTING #
               fmt.trim_whitespace.with({
@@ -92,21 +93,14 @@ packer.startup(function()
               -- 2. prettierd should come first to prevent occassional race condition
               fmt.prettierd,
               fmt.eslint_d,
-              -- fmt.prettier.with({
-              --     extra_args = {
-              --         '--tab-width=4',
-              --         '--trailing-comma=es5',
-              --         '--end-of-line=lf',
-              --         '--arrow-parens=always',
-              --     },
-              -- }),
               fmt.rustfmt,
               fmt.stylua,
-              fmt.gofmt,
+              fmt.goimports,
               fmt.shfmt.with({
                   extra_args = { '-i', 4, '-ci', '-sr' },
               }),
               -- # DIAGNOSTICS #
+              nls.builtins.diagnostics.golangci_lint,
               dgn.eslint_d,
               dgn.shellcheck,
               dgn.luacheck.with({
@@ -162,3 +156,67 @@ function fmt_on_save(client, buf)
         })
     end
 end
+
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+end)
+
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+  'rust_analyzer',
+  'gopls',
+})
+
+-- Fix Undefined global 'vim'
+lsp.nvim_workspace()
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
